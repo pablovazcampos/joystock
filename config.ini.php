a3b82bd4633b157150c1eae8b88f9d5498db0fbf
Pablo Campos <pablocampos@outlook.com>
2014-03-08 11:02:57 -0300

    first commit

---
+++ config.ini.php
@@ -0,0 +1,33 @@
+[EMPRESA]
+
+[BD]
+HOST = "mysql02.divinopolisplacas.hospedagemdesites.ws"
+BANCO_DADOS = "divinopolispla1"
+BD_BACKUP   = "/home/storage/6/60/7f/divinopolisplacas/public_html/joystock/Backup/"
+USUARIO = "divinopolispla1"
+SENHA = "banco262876"
+Y2xpMjMwNDMz
+
+[VERSAO]
+versao = 2.5
+
+[PATH]
+PATH_FUNCAO = "/home/storage/6/60/7f/divinopolisplacas/public_html/joystock/pcte-common"
+PATH_LOGO = "placa.jpg"
+PATH_PRINT = "print"
+
+[TEMPO]
+TMP_EXPIRA = 3600
+
+[LIC]
+NAME = "SG-00001.lic"
+
+[DADOSPADRAO]
+CEP = ""
+CDE = "Rio de Janeiro"
+UNF = "MG"
+PAS = "Brasil"
+NTL = "Rio de Janeiro"
+NCL = "Brasileira"
+OES = "SSPMG"
+COR = "181511"
